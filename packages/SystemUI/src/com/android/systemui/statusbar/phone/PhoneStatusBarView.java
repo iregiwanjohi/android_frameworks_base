bdc28bd4da86643204cf0d32309c053589823c41
Byunghun Jeon <bjeon@codeaurora.org>
2014-12-05 18:44:11 -0800

    WindowManager: Upper level changes to expose blur effect
    Upper level changes to expose blur-behind and blur-mask effect
    
    WindowManager: Adding template for BlurLayer file
    
    Cyanogen changes:
      * Use config.xml instead of settings
      * Don't break stuff if not supported in SF
      * Incremental fade for insecure lockscreen
      * Disabled extra effects for now
    
    Change-Id: I6c97f53395072c4c3a85b37846914c45da0cfaa0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -175,5 +175,6 @@ public class PhoneStatusBarView extends PanelBar {
         super.panelExpansionChanged(panel, frac, expanded);
         mScrimController.setPanelExpansion(frac);
         mBar.updateCarrierLabelVisibility(false);
+        mBar.setBlur(frac);
     }
 }
